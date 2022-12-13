c29cc3d4a24e690e3b23f6443710625d04452df6
Mwangi-Nduta <117742354+Mwangi-Nduta@users.noreply.github.com>
2022-12-14 00:34:18 +0300

    Update 100-print_comb3.c
    positive

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,28 +12,24 @@ int main(void)
 
 	for (m = '0'; m <= '9'; m++)
 	{
-								
 		for (n = '0'; n <= '9'; n++)
-								
 		{
-								
 			if (!((n == m) || (m > n)))
-			
 			{
-								
 				putchar(m);
 				putchar(n);
 				if (!(n == '9' && m == '8'))
-								
 				{
-													putchar(',');
-													putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
-										
 	}
 	putchar('\n');
 
 	return (0);
 }
+													putchar(',');
+													putchar(' ');
+		
