feb3d73f2ce2f72b6d4695b56ad4cfd9a2b81b05
Mwangi-Nduta <117742354+Mwangi-Nduta@users.noreply.github.com>
2022-12-19 23:32:57 +0300

    Update _putchar.c
    change

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: char to be printed
- *    *
- *     * Return: On success 1
- *      */
+ * _putchar - writes the character c to stdout
+ * @c: char to be printed
+ *
+ * Return: On success 1
+ */
 
 int _putchar(char c)
 {
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
