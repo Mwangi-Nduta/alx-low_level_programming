8ee260b32dce4b3ad9bd4cf0fae5a5e85beca96e
Mwangi-Nduta <117742354+Mwangi-Nduta@users.noreply.github.com>
2022-12-23 00:11:08 +0300

    Update 1-strncat.c
    add

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * _strcat - concentrate the string pointed to @src, terminate
- * at null byte, end at @dest;
- * @dest: A pointer to the string to be concentrated upon
- * @src: The source string to be appended to @dest;
- * Return: A pointer to the destination string @dest;
+ * _strncat - concentrates two strings using an input
+ * number of bytes from src
+ * @dest: The string to be appended
+ * @src: The string to append @dest;
+ * @n: The number of bytes from src to appende to dest
+ *
+ * Return: A pointer to the resulting string dest.
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index = 0;
+	int dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
-
 }
